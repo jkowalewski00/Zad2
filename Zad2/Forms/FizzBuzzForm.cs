69e912bff55b920109652734ea49a814b6b77469
kevvol <jakubkowalewski48@gmail.com>
2023-04-04 20:20:45 +0200

    End

--- Zad2/Forms/FizzBuzzForm.cs
+++ Zad2/Forms/FizzBuzzForm.cs
@@ -1,6 +1,18 @@
-﻿namespace Zad2.Forms;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Zad2.Forms;
+using System.ComponentModel.DataAnnotations;
+
 
 public class FizzBuzzForm
 {
+    
+    [Display(Name = "Twój szczęśliwy numerek")]
+    [Required, Range(1, 1000, ErrorMessage = "Oczekiwana wartość {0} z zakresu {1} i {2}.")]
+
+    public int? Number { get; set; }
+    
+    
+    
     
 }
--- Zad2/Pages/Index.cshtml.cs
+++ Zad2/Pages/Index.cshtml.cs
@@ -1,19 +1,70 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Zad2.Forms;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+
 
 namespace Zad2.Pages;
 
 public class IndexModel : PageModel
 {
     private readonly ILogger<IndexModel> _logger;
+    
+    [BindProperty]
+    
+    public FizzBuzzForm FizzBuzz { set; get; }
+    
+    [BindProperty(SupportsGet = true)]
+
+    public string Name { set; get; }
+    
+    public string Response { get; set; }
 
     public IndexModel(ILogger<IndexModel> logger)
     {
         _logger = logger;
     }
 
+    public IActionResult OnPost()
+    {
+        int? number = FizzBuzz.Number;
+        
+        // if (!ModelState.IsValid)
+        // {
+        //     Response = "Podana wartość jest nieprawidłowa.";
+        //     return Page();
+        // }
+        
+        if (number % 3 == 0 && number % 5 == 0)
+        {
+            Response = "FizzBuzz";
+            return Page();
+        }
+        
+        else if (number % 3 == 0)
+        {
+            Response = "Fizz";
+            return Page();
+        }
+        else if (number % 5 == 0)
+        {
+            Response = "Buzz";
+            return Page();
+        }
+        else
+        {
+            Response = $"Liczba {number} nie spełnia kryteriów FizzBuzz";
+            return Page();
+        }
+
+        return Page();
+    }
+    
     public void OnGet()
     {
-
+        if (string.IsNullOrWhiteSpace(Name))
+        {
+            Name = "User";
+        }
     }
+    
 }
